dc37b1f78c7c31834ce1673d970f684b25c666eb
shabbirumrali <shabbirumrali@gmail.com>
2023-09-18 17:27:37 +0530

    created pages of contact, about, gallery, login, home also styled nav using backdrop filter

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Article } from './article/Article';
 export { default as UserProfile } from './userprofile/UserProfile';
+export { default as Navigation } from './Navigation';
---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// styles
+import './pages.css';
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
